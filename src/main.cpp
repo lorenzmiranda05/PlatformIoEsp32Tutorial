3e9072baa8b0242f83407a4a982bb3e06868afe8
Lorenz <lorenzmiranda05@gmail.com>
2022-08-31 22:11:32 +0800

    Updated main.cpp file
    Added blink code with print

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,16 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(921600);
+  Serial.println("Hello from the setup");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Hello from the loop");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
 }
